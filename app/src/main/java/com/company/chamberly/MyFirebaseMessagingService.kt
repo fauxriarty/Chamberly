be439e381862bd626fb7fa8d7e0223d9693f0102
fauxriarty <aditya.chheda04@gmail.com>
2023-11-20 22:17:43 +0530

    fcm tokens being created and fetched kinda now

--- app/src/main/java/com/company/chamberly/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/company/chamberly/MyFirebaseMessagingService.kt
@@ -1,99 +1,103 @@
 package com.company.chamberly
 
+import android.app.ActivityManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.os.Build
-import android.util.Log
 import android.media.RingtoneManager
+import android.util.Log
 import androidx.core.app.NotificationCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import okhttp3.Call
-import okhttp3.Callback
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody.Companion.toRequestBody
-import okhttp3.Response
-import okio.IOException
-import org.json.JSONObject
+private const   val channelName = "My Notification Channel"
+private const val channelId = "channel_id"
+class MessagingService : FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.i("token", token)
 
-class MyFirebaseMessagingService : FirebaseMessagingService() {
+        // Get the current user's UID or identifier
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+
+        if (userId != null) {
+            // Define a reference to the Cloud Firestore
+            val firestore = Firebase.firestore
+            val userRef = firestore.collection("Accounts").document(userId)
+
+            // Update the FCM token for the current user
+            userRef.update("FCMTOKEN", token)
+                .addOnSuccessListener {
+                    Log.i("FCM Token Updated", "Token updated successfully")
+                }
+                .addOnFailureListener {
+                    Log.e("FCM Token Update Failed", it.message ?: "An error occurred while updating FCM token")
+                }
+        }
+    }
+
+
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        Log.d(TAG, "From: ${remoteMessage.from}")
+        super.onMessageReceived(remoteMessage)
+        // Handle incoming message here
 
-        // Check if message contains a data payload.
-        remoteMessage.data.isNotEmpty().let {
-            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+        val title = remoteMessage.data["title"]
+        val body = remoteMessage.data["body"]
+        val groupChatId = remoteMessage.data["groupChatId"]
+        val groupTitle  = remoteMessage.data["groupTitle"]
+        val authorUid  = remoteMessage.data["authorUID"]
+        val authorName = remoteMessage.data["authorName"]
+        // You can customize the notification's behavior here
+        //  if(ChatActivity().isFinishing || ChatActivity().isDestroyed){
+        if(!isAppInForeground()) {
+            sendNotification(title, body, groupChatId, groupTitle, authorUid, authorName)
         }
-
-        // Check if message contains a notification payload.
-        remoteMessage.notification?.let {
-            Log.d(TAG, "Message Notification Body: ${it.body}")
-            sendNotification(it.title ?: "New message", it.body ?: "You have a new message")
-        }
+//        }
     }
-
-    override fun onNewToken(token: String) {
-        Log.d(TAG, "Refreshed token: $token")
-        sendRegistrationToServer(token)
+    private fun isAppInForeground():Boolean{
+        val appProcessInfo = ActivityManager.RunningAppProcessInfo()
+        ActivityManager.getMyMemoryState(appProcessInfo)
+        return (appProcessInfo.importance== ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND)
     }
+    private fun sendNotification(title: String?, messageBody: String?,groupChatId:String?,groupTitle:String?,authorUid:String?,authorName:String?) {
+        val intent = Intent(this, ChatActivity::class.java)
+        intent.putExtra("groupChatId", groupChatId)
+        intent.putExtra("groupTitle", groupTitle)
+        intent.putExtra("authorName",authorName)
+        intent.putExtra("authorUID",authorUid)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent,
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE)
 
-    private fun sendNotification(title: String, messageBody: String) {
-        val intent = Intent(this, MainActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        }
-        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
-
-        val channelId = "Default_Channel"
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(com.google.firebase.R.drawable.notification_bg) // Replace with your notification icon
+            .setSmallIcon(androidx.core.R.drawable.notification_bg)
             .setContentTitle(title)
             .setContentText(messageBody)
             .setAutoCancel(true)
             .setSound(defaultSoundUri)
             .setContentIntent(pendingIntent)
 
+
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        // Since Android Oreo, a Notification Channel is required.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channelName = "Default Channel"
-            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+        // NotificationChannel setup (for Android O and above)
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            )
             notificationManager.createNotificationChannel(channel)
         }
 
         notificationManager.notify(0, notificationBuilder.build())
     }
 
-    private fun sendRegistrationToServer(token: String) {
-        // Implement network call to send token to your server
-        val client = OkHttpClient()
-        val requestBody = JSONObject().apply {
-            put("token", token)
-        }.toString().toRequestBody()
-
-        val request = Request.Builder()
-            .url("http://127.0.0.1:8081") // Replace with your server URL
-            .post(requestBody)
-            .build()
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                Log.e(TAG, "Failed to send token to server: $e")
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                Log.d(TAG, "Token sent to server successfully")
-            }
-        })
-    }
-
-    companion object {
-        const val TAG = "MyFirebaseMsgService"
-    }
 }
